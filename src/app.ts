1e4396d293f1908a28ffe32e41929c4e5806f4c3
adiazt01 <adiaztovar2004@gmail.com>
2023-11-05 19:38:05 -0400

    Add morgan logging middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 import { usersRouter } from "./routes/users.routes";
 import { productsRouter } from "./routes/products.routes";
 
+
 export const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
